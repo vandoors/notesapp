89ac450f875b2bf4fdc522f6587bd2d8cd044cc9
vandoors <143453791+vandoors@users.noreply.github.com>
2024-05-02 19:05:27 -0500

    Install and run Prettier

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,9 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "notesapp": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
+   api: {
+      notesapp: {
+         GraphQLAPIEndpointOutput: "string";
+         GraphQLAPIIdOutput: "string";
+         GraphQLAPIKeyOutput: "string";
+      };
+   };
+};
