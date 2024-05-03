241cdb886440701575f45373fbc8a631caa74c74
vandoors <143453791+vandoors@users.noreply.github.com>
2024-05-02 19:59:09 -0500

    Add initial functionality
    Can now view notes...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from "./logo.svg";
+import React, { useEffect, useReducer, useState } from "react";
+import { generateClient } from "aws-amplify/api";
+import { List, Input, Button } from "antd";
+import "antd/dist/reset.css";
+import { listNotes } from "./graphql/queries";
 import "./App.css";
 
-function App() {
+const initialState = {
+   notes: [],
+   loading: true,
+   error: false,
+   form: { name: "", description: "" },
+};
+
+function reducer(state, action) {
+   switch (action.type) {
+      case "SET_NOTES":
+         return { ...state, notes: action.notes, loading: false };
+      case "ERROR":
+         return { ...state, loading: false, error: true };
+      default:
+         return { ...state };
+   }
+}
+
+const App = () => {
+   const [state, dispatch] = useReducer(reducer, initialState);
+
+   const client = generateClient();
+
+   const fetchNotes = async () => {
+      try {
+         const notesData = await client.graphql({
+            query: listNotes,
+         });
+         dispatch({
+            type: "SET_NOTES",
+            notes: notesData.data.listNotes.items,
+         });
+      } catch (err) {
+         console.error(err);
+         dispatch({ type: "ERROR" });
+      }
+   };
+
+   useEffect(() => {
+      fetchNotes();
+   }, []);
+
+   const styles = {
+      container: { padding: 20 },
+      input: { marginBottom: 10 },
+      item: { textAlign: "left" },
+      p: { color: "#1890ff" },
+   };
+
+   function renderItem(item) {
+      return (
+         <List.Item style={styles.item}>
+            <List.Item.Meta title={item.name} description={item.description} />
+         </List.Item>
+      );
+   }
+
    return (
-      <div className="App">
-         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-               Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-               className="App-link"
-               href="https://reactjs.org"
-               target="_blank"
-               rel="noopener noreferrer"
-            >
-               Learn React
-            </a>
-         </header>
+      <div className="App" style={styles.container}>
+         <List
+            loading={state.loading}
+            dataSource={state.notes}
+            renderItem={renderItem}
+         />
       </div>
    );
-}
+};
 
 export default App;
