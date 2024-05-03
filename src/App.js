697881e36302055b8500a127d33e147e9010ace3
vandoors <143453791+vandoors@users.noreply.github.com>
2024-05-02 20:32:29 -0500

    Implement note deletion functionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import { v4 as uuid } from "uuid";
 import { List, Input, Button } from "antd";
 import "antd/dist/reset.css";
 import { listNotes } from "./graphql/queries";
-import { createNote as CreateNote } from "./graphql/mutations";
+import {
+   createNote as CreateNote,
+   deleteNote as DeleteNote,
+} from "./graphql/mutations";
 import "./App.css";
 
 const CLIENT_ID = uuid();
@@ -91,6 +94,25 @@ const App = () => {
       });
    };
 
+   const deleteNote = async ({ id }) => {
+      const index = state.notes.findIndex((n) => n.id === id);
+      const notes = [
+         ...state.notes.slice(0, index),
+         ...state.notes.slice(index + 1),
+      ];
+
+      dispatch({ type: "SET_NOTES", notes });
+
+      try {
+         await client.graphql({
+            query: DeleteNote,
+            variables: { input: { id } },
+         });
+      } catch (err) {
+         console.error(err);
+      }
+   };
+
    useEffect(() => {
       fetchNotes();
    }, []);
@@ -104,7 +126,14 @@ const App = () => {
 
    function renderItem(item) {
       return (
-         <List.Item style={styles.item}>
+         <List.Item
+            style={styles.item}
+            actions={[
+               <p style={styles.p} onClick={() => deleteNote(item)}>
+                  Delete
+               </p>,
+            ]}
+         >
             <List.Item.Meta title={item.name} description={item.description} />
          </List.Item>
       );
